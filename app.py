caaae94050ec0a1a076d748fbb2cd4f55ef64217
Adriane James J. Puzon <adrianejames27@gmail.com>
2022-03-19 23:13:43 +0800

    removed unnecessary imports in app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
-import random
 import logging
 import discord
 from discord import Option
-from discord.ext import commands
 from keep_alive import keep_alive
 
 # SET DEFAULT BOT SETTINGS & GLOBAL VARS
